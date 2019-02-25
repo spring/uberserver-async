d655e4207bb9034394e5aec01fd2fab23beacd0b
TurBoss <j.l.toledano.l@gmail.com>
2019-02-25 22:35:18 +0100

    remove unused

--- plugins/core.py
+++ plugins/core.py
@@ -8,11 +8,11 @@ import logging
 
 log = logging.getLogger(__name__)
 
-ping_clients = []
+ping_client = None
 
 
 def _pong(message):
-    message.client.writeln("PONG {}".format(message.params[0]))
+    pass
 
 
 def _redispatch_message_common(message, mtype):
@@ -71,60 +71,12 @@ def _redispatch_nick(message):
     signal("nick").send(message, user=old_user, new_nick=new_nick)
 
 
-def _parse_mode(message):
-    # :ChanServ!ChanServ@services. MODE ##fwilson +o fwilson
-    if "CHANMODES" in message.client.server_supports:
-        argument_modes = "".join(message.client.server_supports["CHANMODES"].split(",")[:-1])
-        argument_modes += message.client.server_supports["PREFIX"].split(")")[0][1:]
-    else:
-        argument_modes = "beIqaohvlk"
-    log.info("argument_modes are", argument_modes)
-    user = get_user(message.source)
-    channel = message.params[0]
-    modes = message.params[1]
-    args = message.params[2:]
-    flag = "+"
-    for mode in modes:
-        if mode in "+-":
-            flag = mode
-            continue
-        if mode in argument_modes:
-            arg = args.pop(0)
-        else:
-            arg = None
-        signal("{}mode".format(flag)).send(message, mode=mode, arg=arg, user=user, channel=channel)
-        signal("mode {}{}".format(flag, mode)).send(message, arg=arg, user=user, channel=channel)
-
-
-def _server_supports(message):
-    supports = message.params[1:-1]  # No need for "Are supported by this server" or bot's nickname
-    log.info("Server supports {}".format(supports))
-    for feature in supports:
-        if "=" in feature:
-            k, v = feature.split("=")
-            message.client.server_supports[k] = v
-        else:
-            message.client.server_supports[feature] = True
-
-
-def _nick_in_use(message):
-    message.client.old_nickname = message.client.nickname
-    s = message.client.nick_in_use_handler()
-
-    def callback():
-        message.client.nickname = s
-        message.client.writeln("NICK {}".format(s))
-
-    # loop.call_later(5, callback)
-
-
 def _ping_servers():
-    for client in ping_clients:
-        if client.last_pong != 0 and time.time() - client.last_pong > 90:
-            del ping_clients[:]
-            client.connection_lost(Exception())
-        client.writeln("PING")
-        client.last_ping = time.time()
+    if ping_client.last_pong != 0 and time.time() - ping_client.last_pong > 90:
+        ping_client.connection_lost(Exception())
+        return
+    ping_client.writeln("PING")
+    ping_client.last_ping = time.time()
 
     asyncio.get_event_loop().call_later(29, _ping_servers)
 
@@ -157,7 +109,9 @@ def _login_client(client):
 
 
 def _queue_ping(client):
-    ping_clients.append(client)
+    global ping_client
+
+    ping_client = client
     _ping_servers()
 
 
@@ -197,16 +151,6 @@ def _redispatch_agreement(message):
     signal("agreement_end").send(message)
 
 
-def _redispatch_bridged_client(message):
-    log.debug("BRIDGED CLIENT")
-    log.debug(message)
-
-
-def _redispatch_un_bridged_client(message):
-    log.debug("UNBRIDGETD CLIENT FROM")
-    log.debug(message)
-
-
 def _redispatch_joined_from(message):
     log.debug("JOINED FROM")
     log.debug(message)
@@ -222,21 +166,11 @@ def _redispatch_said_from(message):
     log.debug(message)
 
 
-def _redispatch_clients_from(message):
-    log.debug("CLIENTS FROM")
-    log.debug(message)
-
-
-def _redispatch_logininfoend(message):
-    signal("logininfoend").send(message)
-
-
 signal("raw").connect(_redispatch_raw)
 signal("spring").connect(_redispatch_spring)
 
 signal("connected").connect(_login_client)
 
-signal("spring-ping").connect(_pong)
 signal("spring-pong").connect(_catch_pong)
 
 signal("spring-said").connect(_redispatch_said)
@@ -252,21 +186,14 @@ signal("spring-removeuser").connect(_redispatch_removeuser)
 signal("spring-quit").connect(_redispatch_quit)
 signal("spring-kick").connect(_redispatch_kick)
 signal("spring-nick").connect(_redispatch_nick)
-signal("spring-mode").connect(_parse_mode)
-signal("spring-005").connect(_server_supports)
 signal("spring-accepted").connect(_connection_registered)
 signal("spring-denied").connect(_connection_denied)
 signal("spring-agreementend").connect(_redispatch_agreement)
 
 signal("spring-motd").connect(_parse_motd)
-signal("spring-logininfoend").connect(_redispatch_logininfoend)
 signal("spring-clients").connect(_redispatch_clients)
-
-signal("spring-bridgedclientfrom").connect(_redispatch_bridged_client)
-signal("spring-unbridgedclientfrom").connect(_redispatch_un_bridged_client)
 
 signal("spring-joinedfrom").connect(_redispatch_joined_from)
 signal("spring-leftfrom").connect(_redispatch_left_from)
 
 signal("spring-saidfrom").connect(_redispatch_said_from)
-signal("spring-clientsfrom").connect(_redispatch_clients_from)
